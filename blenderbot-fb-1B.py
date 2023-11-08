f853623d1401c7404da4942c62337deb3928d7f0
sohfix <nicolas.tech@outlook.com>
2023-11-08 03:16:23 -0600

    blenderbot and gpt-2-m work -n

--- blenderbot-fb-1B.py
+++ blenderbot-fb-1B.py
@@ -1,37 +1,3 @@
-import threading
-import itertools
-import time
-import sys
-from printy import inputy, printy
-from Blender import BlenderbotPipeline
-
-
-class LoadingBar:
-    def __init__(self, delay=0.1):
-        self.delay = delay  # In seconds
-        self.spinner_signs = itertools.cycle(["-", "\\", "|", "/"])
-        self.loading = False
-
-    def spinner_task(self):
-        while self.loading:
-            sys.stdout.write(next(self.spinner_signs))  # write the next character
-            sys.stdout.flush()  # flush stdout buffer (actual character display)
-            time.sleep(self.delay)
-            sys.stdout.write("\b")  # erase the last written char
-
-    def start(self, model):
-        printy(f"Loading model: {model}")
-        self.loading = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def stop(self):
-        self.loading = False
-        time.sleep(self.delay)
-        sys.stdout.write("\b \b")  # erase the spinner and leave the cursor at the start
-
-
-# Now we modify the usage of the BlenderbotPipeline to include the loading animation
-
 # Initialize the loading bar
 loading_bar = LoadingBar()
 model = "facebook/blenderbot-1B-distill"
--- main.py
+++ main.py
@@ -1,1 +1,1 @@
-# todo
+print("fix")
